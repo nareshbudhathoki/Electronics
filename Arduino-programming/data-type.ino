0dfd1b577ad5a4fc848dc9aca607d2b90fbf979f
Naresh Budhathoki <72507640+nareshbudhathoki@users.noreply.github.com>
2025-08-03 08:40:45 +0000

    data type in the arduino

---
+++ Arduino-programming/data-type.ino
@@ -0,0 +1,20 @@
+// data types in the arduino
+int val = 10; //can store negative and positive value same as short
+byte unsign = 55; //0 -255 positive value only. unsigned value only
+word var = 3545; // 0 - 65535 unsigned value only
+short var2 = 32767; // this stores signed bit from -32768 - 32767
+long var3 = 423452345; // -2 billion to + 2 billion
+float var4 = 45.34; // float is used store the decimal value
+double var5 = 34534.53; // it stores the largest decimal value
+unsigned int var6 = 65535; // only store the positive value from 0 - 65535
+unsigned long var7 = 254442; //32 bit positive value
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
